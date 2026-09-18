b5b045f196bc28c90c924f405b41a9a6a2699c9e
thien952006 <thienbui2006hd@gmail.com>
2026-09-18 16:42:57 +0000

    add About page

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 
 const app = express();
+app.set("view engine", "ejs");
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, web!');
 });
-app.get('/about', (req, res) => {
+app.get('/introduction', (req, res) => {
   res.send('Hello, Im Thien');
+});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
 app.listen(PORT, () => {
